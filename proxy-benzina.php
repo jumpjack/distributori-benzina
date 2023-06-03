8997a486332a0fffe044b77aefaae5eb41d50b39
jumpjack <jumpjack@libero.it>
2023-06-03 19:42:53 +0200

    Add files via upload

--- proxy-benzina.php
+++ proxy-benzina.php
@@ -3,12 +3,14 @@
 $urlArea = "https://carburanti.mise.gov.it/ospzApi/search/area";
 $urlZone= "https://carburanti.mise.gov.it/ospzApi/search/zone";
 
+$default=true;
 
 $myRegion = -1;
 if ( $_GET['region'] == "" ) {
   //$myRegion = 9;
 } else {
   $myRegion = $_GET['region'];
+  $default=false;
 }
 
 $myProvince = "";
@@ -16,6 +18,7 @@ if ( $_GET['province'] == "" ) {
   //$myProvince = "RM";
 } else {
   $myProvince = $_GET['province'];
+  $default=false;
 }
 
 $myTown = "";
@@ -23,6 +26,7 @@ if ( $_GET['town'] == "" ) {
   //$myTown = "Fonte Nuova";
 } else {
   $myTown = $_GET['town'];
+  $default=false;
 }
 
 
@@ -31,6 +35,7 @@ if ( $_GET['radius'] == "" ) {
   $myRadius = 3;
 } else {
   $myRadius = $_GET['radius'];
+  $default=false;
 }
 
 
@@ -39,6 +44,7 @@ if ( $_GET['lat'] == "" ) {
   $myLat = 42.02263108084876;
 } else {
   $myLat = $_GET['lat'];
+  $default=false;
 }
 
 
@@ -47,6 +53,7 @@ if ( $_GET['lng'] == "" ) {
   $myLng = 12.63365339487791;
 } else {
   $myLng = $_GET['lng'];
+  $default=false;
 }
 
 
@@ -65,9 +72,8 @@ if ( $myTown !==   "") {
 } else {
     $data = '{"points":[{"lat" : ' . $myLat . ' ,"lng": ' . $myLng . ' }],"fuelType":"1-1","priceOrder":"asc","radius": ' . $myRadius . '}';
     $url = $urlZone;
+  $default=false;
 }
-
-
 
 $curl = curl_init($url);
 curl_setopt($curl, CURLOPT_URL, $url);
@@ -79,7 +85,7 @@ $headers = array(
    'Access-Control-Allow-Origin: *',
    'Access-Control-Allow-Credentials: true',
    'Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE',
-   'Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With, X-GIGYA-ID_TOKEN,  X-GIGYA-ID-TOKEN, apikey, *, expiration, json, fields, login_token, authorization, access_token, X-RESUME-TOKEN, Content-Type'
+   'Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With, X-GIGYA-ID_TOKEN,  X-GIGYA-ID-TOKEN, apikey, *, expiration, json, fields, login_token, authorization, access_token, X-RESUME-TOKEN, content-type'
 );
 
 		header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
@@ -101,20 +107,18 @@ curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 $resp = curl_exec($curl);
 curl_close($curl);
 
-echo '{ <br>
-     "sent" : {<br>
-       "region" : "' . $_GET['region'] .  '",<br>
-       "province" : "' . $_GET['province'] .  '",<br>
-       "town" : "' . $_GET['town'] .  '",<br>
-       "radius" : ' . $_GET['radius'] .  ',<br>
-       "lat" : ' . $_GET['lat'] .  ',<br>
-       "lng" : ' . $_GET['lng'] .  '<br>
-     },
+
+echo '{ 
+     "sent" :' .  $data . ',
+     "url" : "' . $url . '",
      "response" : ';
 
-echo $resp;
+if ($resp) {
+  echo $resp;
+} else {
+  echo '""';
+}
 
 echo '}';
 
 ?>
-
